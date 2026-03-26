7316f384a9f77c80603e5c5cd618271a32a23385
Peter_Eisenchenk <peter.jun@eisenschenk.at>
2026-03-26 19:43:41 +0100

    Initial Commit

---
+++ NewFileCreator/NewFileCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewFileCreatorApp.swift
+//  NewFileCreator
+//
+//  Created by Peter on 26.03.26.
+//
+
+import SwiftUI
+
+@main
+struct NewFileCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
